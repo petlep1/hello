6f01f889aaf4a4135f1b9c87cff81633cd55b772
petlep1 <ppetley@gmail.com>
2015-10-13 18:03:04 +0100

    First commit

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Peter on 10/12/2015.
+ */
+package main.java.hello;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Peter on 10/12/2015.
+ */
+package main.java.hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
